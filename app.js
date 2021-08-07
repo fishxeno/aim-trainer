d53a9d762d70f3c3897e728049ee7aaa03a9e79c
Wang Wei Yu <wangweiyu94791358@gmail.com>
2021-08-08 00:15:07 +0800

    console

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ function storeSessions(data) {
 }
 
 function returnSessions() {
+    console.log("reading")
     fs.readFile('sessions.json', 'utf-8', (err, data) => {
         if (err) {
             throw err;
@@ -29,6 +30,7 @@ function returnSessions() {
 
         // parse JSON object
         const sessions = JSON.parse(data.toString());
+        
         return sessions;
     });
 }
